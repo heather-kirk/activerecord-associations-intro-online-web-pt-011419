4069f2d80506ec7272d9fa4017234f2f15f7268f
Heather Kirk <hskirk@gmail.com>
2019-04-11 01:10:51 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -2,7 +2,6 @@ class CreateGenres < ActiveRecord::Migration[4.2]
   
   def change 
     create_table :genres do |t|
-      create_table :genres do |t|
         t.string :name 
       end 
     end 
